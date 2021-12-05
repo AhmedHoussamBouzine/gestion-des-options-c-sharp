445af6c98a5c9ec5bc3cec5ee6ff1e7e35fc3313
AhmedHoussamBouzine <92208029+AhmedHoussamBouzine@users.noreply.github.com>
2021-12-05 13:56:51 +0100

    Update Program.cs

--- gestion des filieres/Program.cs
+++ gestion des filieres/Program.cs
@@ -1,18 +1,30 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace gestion_des_filieres
 {
     class Program
     {
+
+        // d'abord on va creer une sturcture qui contient chaque etudiant et leurs choix 
+
+        struct choix
+        {
+           public Etudiant et;
+           public string op1;
+           public string op2;
+           public string op3;
+
+        }
+
         static void Main(string[] args)
         {
             // le nombre des etudiants
             Console.WriteLine("entrer le nombre des etudiants ");
-            int j = int.Parse(Console.ReadLine());
+            int nbr = int.Parse(Console.ReadLine());
 
             // declaration du tableau d'affichage
-            string[] tab = new string[j];
+            string[] tab = new string[nbr];
 
 
             //declarations des trois options : gl , abd, asr
@@ -21,12 +33,15 @@ namespace gestion_des_filieres
             option asr = new option("asr", 20);
 
             //declaration de la liste des etudiants 
-            var liste = new List<Tuple<Etudiant, string, string, string>>();
+            var liste = new List<choix>();
 
 
             // la liste d'etudiants 
-            for (int i = 0; i < j; i++)
+            for (int i = 0; i < nbr; i++)
             {
+
+                choix cho;
+                //les informations sur l'etudiant 
                 Console.WriteLine("entrer le nom d'etudiant N" + (i+1));
                 string nom = Convert.ToString(Console.ReadLine());
                 Console.WriteLine("entrer le prenom d'etudiant N" + (i + 1));
@@ -35,77 +50,77 @@ namespace gestion_des_filieres
                 string cne = Convert.ToString(Console.ReadLine());
                 Console.WriteLine("entrer la note d'etudiant N" + (i + 1));
                 double note = double.Parse(Console.ReadLine());
+                //les choix de l'etudiant 
                 Console.WriteLine("entrer le premier choix d'etudiant N" + (i + 1));
-                string op1 = Convert.ToString(Console.ReadLine());
+                cho.op1 = Convert.ToString(Console.ReadLine());
                 Console.WriteLine("entrer le 2eme choix d'etudiant N" + (i + 1));
-                string op2 = Convert.ToString(Console.ReadLine());
+                cho.op2 = Convert.ToString(Console.ReadLine());
                 Console.WriteLine("entrer le 3eme choix d'etudiant N" + (i + 1));
-                string op3 = Convert.ToString(Console.ReadLine());
+                cho.op3 = Convert.ToString(Console.ReadLine());
                 Console.WriteLine("----------------------------------");
-
-                Etudiant etudiant = new Etudiant(nom, prenom, cne, note);
-
-                var tuple = Tuple.Create(etudiant, op1, op2, op3);
-                liste.Add(tuple);
+                Etudiant etudiant = new Etudiant(nom, prenom, cne, note);  
+                cho.et = etudiant;
+                liste.Add(cho);
             }
            
             // Tri de la liste des etudiants selon les notes  
 
-            liste.Sort(delegate (Tuple<Etudiant, string, string, string> x, Tuple<Etudiant, string, string, string> y) {
-                return y.Item1.Enote.CompareTo(x.Item1.Enote);
+            liste.Sort(delegate (choix x, choix y) {
+                return y.et.Enote.CompareTo(x.et.Enote);
             });
+
             
             // Affectation des options 
 
-            for (int i =0; i < j; i++) { 
+            for (int i = 0; i < nbr; i++) { 
                    
-                    switch (liste[i].Item2)
+                    switch (liste[i].op1)
                     {
                         case "gl":
                             if (gl.placedispo > 0)
                             {
-                                tab[i] = liste[i].Item1.Enom +"  "+ liste[i].Item1.Eprenom + "  " + liste[i].Item1.Ecne + "  " + "  -->  " + gl.onom ;
+                                tab[i] = liste[i].et.Enom +"  "+ liste[i].et.Eprenom + "  " + liste[i].et.Ecne + "  " + "  -->  " + gl.onom ;
                                 --gl.placedispo;
                             }
                             break;
                         case "abd":
                             if (abd.placedispo > 0)
                             {
-                                tab[i] = liste[i].Item1.Enom + "  " + liste[i].Item1.Eprenom + "  " + liste[i].Item1.Ecne + "  " + "  -->  " + abd.onom;
+                                tab[i] = liste[i].et.Enom + "  " + liste[i].et.Eprenom + "  " + liste[i].et.Ecne + "  " + "  -->  " + abd.onom;
                                 --abd.placedispo;
                             }
                             break;
                         case "asr":
                             if (asr.placedispo > 0)
                             {
-                                tab[i] = liste[i].Item1.Enom + "  " + liste[i].Item1.Eprenom + "  " + liste[i].Item1.Ecne + "  " + "  -->  " + asr.onom;
+                                tab[i] = liste[i].et.Enom + "  " + liste[i].et.Eprenom + "  " + liste[i].et.Ecne + "  " + "  -->  " + asr.onom;
                                 --asr.placedispo;
                             }
                             break;
                     }
                     if (tab[i] == null)
                     {
-                        switch (liste[i].Item3)
+                        switch (liste[i].op2)
                         {
 
                         case "gl":
                             if (gl.placedispo > 0)
                             {
-                                tab[i] = liste[i].Item1.Enom + "  " + liste[i].Item1.Eprenom + "  " + liste[i].Item1.Ecne + "  " + "  -->  " + gl.onom;
+                                tab[i] = liste[i].et.Enom + "  " + liste[i].et.Eprenom + "  " + liste[i].et.Ecne + "  " + "  -->  " + gl.onom;
                                 --gl.placedispo;
                             }
                             break;
                         case "abd":
                             if (abd.placedispo > 0)
                             {
-                                tab[i] = liste[i].Item1.Enom + "  " + liste[i].Item1.Eprenom + "  " + liste[i].Item1.Ecne + "  " + "  -->  " + abd.onom;
+                                tab[i] = liste[i].et.Enom + "  " + liste[i].et.Eprenom + "  " + liste[i].et.Ecne + "  " + "  -->  " + abd.onom;
                                 --abd.placedispo;
                             }
                             break;
                         case "asr":
                             if (asr.placedispo > 0)
                             {
-                                tab[i] = liste[i].Item1.Enom + "  " + liste[i].Item1.Eprenom + "  " + liste[i].Item1.Ecne + "  " + "  -->  " + asr.onom;
+                                tab[i] = liste[i].et.Enom + "  " + liste[i].et.Eprenom + "  " + liste[i].et.Ecne + "  " + "  -->  " + asr.onom;
                                 --asr.placedispo;
                             }
                             break;
@@ -114,56 +129,50 @@ namespace gestion_des_filieres
                     }
                     if (tab[i] == null)
                     {
-                        switch (liste[i].Item4)
+                        switch (liste[i].op3)
                         {
 
                         case "gl":
                             if (gl.placedispo > 0)
                             {
-                                tab[i] = liste[i].Item1.Enom + "  " + liste[i].Item1.Eprenom + "  " + liste[i].Item1.Ecne + "  " + "  -->  " + gl.onom;
+                                tab[i] = liste[i].et.Enom + "  " + liste[i].et.Eprenom + "  " + liste[i].et.Ecne + "  " + "  -->  " + gl.onom;
                                 --gl.placedispo;
                             }
                             break;
                         case "abd":
                             if (abd.placedispo > 0)
                             {
-                                tab[i] = liste[i].Item1.Enom + "  " + liste[i].Item1.Eprenom + "  " + liste[i].Item1.Ecne + "  " + "  -->  " + abd.onom;
+                                tab[i] = liste[i].et.Enom + "  " + liste[i].et.Eprenom + "  " + liste[i].et.Ecne + "  " + "  -->  " + abd.onom;
                                 --abd.placedispo;
                             }
                             break;
                         case "asr":
                             if (asr.placedispo > 0)
                             {
-                                tab[i] = liste[i].Item1.Enom + "  " + liste[i].Item1.Eprenom + "  " + liste[i].Item1.Ecne + "  " + "  -->  " + asr.onom;
+                                tab[i] = liste[i].et.Enom + "  " + liste[i].et.Eprenom + "  " + liste[i].et.Ecne + "  " + "  -->  " + asr.onom;
                                 --asr.placedispo;
                             }
                             break;
                     }
 
-                    
                 }
             
-
             }
-            
+             
             // affichage de la liste finale
 
-            Console.WriteLine("La liste des etudiants avec leurs filieres : ");
-            Console.WriteLine("Nom   Prenom   CNE   Filiere");
+            Console.WriteLine("La liste des etudiants  : ");
+            Console.WriteLine(" Nom   |  Prenom  |  CNE  |   Filiere ");
             Console.Write("\n");
-           
 
-            for (int i = 0; i <j ; i++)
+            for (int i = 0; i < nbr ; i++)
             {
-                
+
                 Console.Write(tab[i] + "\t" );
-               
                 Console.Write("\n");
                 Console.Write("----------------------------------");
                 Console.Write("\n");
-            }
-           
-          
+            }   
         }
     }
 }
